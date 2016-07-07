e3246898e1641cf9837d0a3c1ef17f43fe1c26ad
GB1609 <gbrunetti1708@gmail.com>
2016-07-07 17:46:28 +0200

    Convert Project to Web Project

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,11 +1,14 @@
 package core;
 import java.time.LocalDate;
+import dao.ClientsDao;
+import tables.Clients;
 public class Main
 {
 	public static void main(String[] args)
 	{
 		LocalDate.now();
 		DaoFactory dao=DaoFactory.getDAOFactory(DaoFactory.POSTGRESQL);
-		dao.getClienteDao();
+		ClientsDao i=dao.getClienteDao();
+		i.save(new Clients("gio","bru","c","a",LocalDate.now(),"Cos",10000));
 	}
 }
