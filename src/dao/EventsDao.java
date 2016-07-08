68a077cf96b5e018283dfaa071feedf1f8aa0605
Andrea <Andrea@PC-Andrew>
2016-07-08 22:12:45 +0200

    searchbyCategory fixed.

--- src/dao/EventsDao.java
+++ src/dao/EventsDao.java
@@ -1,19 +1,30 @@
 package dao;
+
 import java.time.LocalDate;
 import java.util.Set;
-import tables.Category;
+
 import tables.Events;
-public interface EventsDao
-{
+
+public interface EventsDao {
 	public void delete(int eventCode);
+
 	public void save(Events e);
-	public Set<Events> searchByCategory(Category c);
+
+	public Set<Events> searchByCategory(int categoryCode);
+
 	public Set<Events> searchByDate(LocalDate l);
+
 	public Set<Events> searchByOrganizator(String organizator);
+
 	public Set<Events> searchByPartecipants(int idpartecipant);
-	public Set<Events> searchByPlace(String name,String city);
-	public Set<Events> searchByPrice(double price,boolean max);
+
+	public Set<Events> searchByPlace(String name, String city);
+
+	public Set<Events> searchByPrice(double price, boolean max);
+
 	public void startSeller(Events e);
+
 	public void stopSeller(Events e);
+
 	public void update(Events e);
 }
